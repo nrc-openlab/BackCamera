096ca22c26d6941cad271031a5ec562fc1f6310d
You_Blue <yc424k@gmail.com>
2023-06-07 03:40:33 +0900

    튕김버그 수정

--- source/lib/main.dart
+++ source/lib/main.dart
@@ -1,4 +1,3 @@
-// 화면 녹화 및 저장 테스트 dart파일
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,17 +9,14 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   requestPermissions() async {
     await PermissionHandler().requestPermissions([
       PermissionGroup.storage,
@@ -46,25 +42,23 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class HomePage extends StatefulWidget {
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   WebViewController _controller;
-  _HomePageState(){
+  String formattedDate;
+  bool recording = false;
+  Directory directory;
+  bool inProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
     startScreenRecord(false);
   }
-  var formattedDate;
-  bool recording = false;
-  var directory = Directory('/storage/emulated/0/DCIM/BackCamera').create(recursive: true);
-  // final Completer<WebViewController> _controller = Completer<WebViewController>();
-
-  bool inProgress = false;
 
   Future<File> moveFile(File sourceFile, String newPath) async {
     try {
@@ -79,40 +73,45 @@ class _HomePageState extends State<HomePage> {
   }
 
   startScreenRecord(bool audio) async {
-    bool start = false;
-    var now = new DateTime.now();
-    var formatter = new DateFormat('yyyy_MM_dd_HH_mm_ss');
+    var now = DateTime.now();
+    var formatter = DateFormat('yyyy_MM_dd_HH_mm_ss');
     formattedDate = formatter.format(now);
     print('-------------');
     print(formattedDate);
     print('-------------');
+    setState(() {
+      recording = true;
+    });
+
     if (audio) {
-      start = await FlutterScreenRecording.startRecordScreenAndAudio(formattedDate);
+      await FlutterScreenRecording.startRecordScreenAndAudio(formattedDate);
     } else {
-      start = await FlutterScreenRecording.startRecordScreen(formattedDate);
+      await FlutterScreenRecording.startRecordScreen(formattedDate);
     }
-    if (start) {
-      setState(() => recording = !recording);
-    }
-    return start;
   }
 
-  stopScreenRecord() async {
+  Future<bool> stopScreenRecord() async {
     String path = await FlutterScreenRecording.stopRecordScreen;
-    setState(() {
-      recording = !recording;
-    });
-    // create '/storage/emulated/0/DCIM/BackCamera/' direct
-    var directory = Directory('/storage/emulated/0/DCIM/BackCamera').create(recursive: true);
-    //final directory = await getTemporaryDirectory();
-    final newPath = '/storage/emulated/0/DCIM/BackCamera/' + "$formattedDate.mp4";
-    final file = await moveFile(File(path), newPath);
+    if (path != null) {
+      setState(() {
+        recording = false;
+      });
+      // create '/storage/emulated/0/DCIM/BackCamera/' directory
+      directory = await Directory('/storage/emulated/0/DCIM/BackCamera')
+          .create(recursive: true);
+      final newPath = '${directory.path}/$formattedDate.mp4';
+      final file = await moveFile(File(path), newPath);
+      return true;
+    } else {
+      return false;
+    }
   }
+
   String button_text = '녹화종료';
-  Color _color=Colors.red;
+  Color _color = Colors.red;
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -120,9 +119,9 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: (){
+            onPressed: () {
               _controller.reload();
-            }
+            },
           )
         ],
       ),
@@ -133,86 +132,90 @@ class _HomePageState extends State<HomePage> {
           ),
           Expanded(
             flex: 9,
-            child:
-            WebView(
-              initialUrl:'http://10.10.10.1:8000/index.html',
+            child: WebView(
+              // initialUrl: 'http://10.10.10.1:8000/index.html',
+              initialUrl: 'https://www.naver.com',
               javascriptMode: JavascriptMode.unrestricted,
               onWebViewCreated: (WebViewController webViewController) {
                 _controller = webViewController;
               },
             ),
           ),
-
           Expanded(
             flex: 1,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                TextButton(onPressed: () async {
-                  final url = Uri.parse(
-                    'http://www.nrc.go.kr/nrc/main.do',
-                  );
-                  if (await canLaunchUrl(url)) {
-                    launchUrl(url, mode:LaunchMode.externalApplication);
-                  } else {
-                    // ignore: avoid_print
-                    print("Can't launch $url");
-                  }
-                },
-                  child: Image.asset('images/nrc.png',width: 100,height: 27,),
+                TextButton(
+                  onPressed: () async {
+                    final url = Uri.parse(
+                      'http://www.nrc.go.kr/nrc/main.do',
+                    );
+                    if (await canLaunch(url.toString())) {
+                      launch(url.toString());
+                    } else {
+                      print("Can't launch $url");
+                    }
+                  },
+                  child: Image.asset(
+                    'images/nrc.png',
+                    width: 100,
+                    height: 27,
+                  ),
                   style: ElevatedButton.styleFrom(
                     primary: Colors.white,
                     onPrimary: Colors.black,
-                    // shape: CircleBorder(),
                   ),
-
                 ),
-                TextButton(onPressed: () async {
-                  final url = Uri.parse(
-                    'http://www.nrc.go.kr/at_rd/web/index.do',
-                  );
-                  if (await canLaunchUrl(url)) {
-                    launchUrl(url, mode:LaunchMode.externalApplication);
-                  } else {
-                    // ignore: avoid_print
-                    print("Can't launch $url");
-                  }
-                },
-                  child: Image.asset('images/opcn.png',width: 100,height: 27,),
+                TextButton(
+                  onPressed: () async {
+                    final url = Uri.parse(
+                      'http://www.nrc.go.kr/at_rd/web/index.do',
+                    );
+                    if (await canLaunch(url.toString())) {
+                      launch(url.toString());
+                    } else {
+                      print("Can't launch $url");
+                    }
+                  },
+                  child: Image.asset(
+                    'images/opcn.png',
+                    width: 100,
+                    height: 27,
+                  ),
                   style: ElevatedButton.styleFrom(
                     primary: Colors.white,
                     onPrimary: Colors.black,
-                    // shape: CircleBorder(),
                   ),
                 ),
-                ElevatedButton(style: ElevatedButton.styleFrom(
-                  primary: _color,
-                ),onPressed: () {
-                  if (button_text == '녹화시작'){
-                    setState(() {
-                      button_text ='녹화종료';
-                      _color=Colors.red;
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: _color,
+                  ),
+                  onPressed: () async {
+                    if (button_text == '녹화시작') {
                       startScreenRecord(false);
-                    });
-                  }
-                  else {
-                    setState(() {
-                      button_text = '녹화시작';
-                      _color=Colors.green;
-                      stopScreenRecord();
-
-                    });
-                  }
-
-                }, child: Text(button_text)),
+                      setState(() {
+                        button_text = '녹화종료';
+                        _color = Colors.red;
+                      });
+                    } else {
+                      bool stopped = await stopScreenRecord();
+                      if (stopped) {
+                        setState(() {
+                          button_text = '녹화시작';
+                          _color = Colors.green;
+                        });
+                      }
+                    }
+                  },
+                  child: Text(button_text),
+                ),
               ],
             ),
           ),
-
         ],
       ),
     );
-
   }
-
 }
